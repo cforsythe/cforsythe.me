6af394423fd158441060eace2b3349db67300471
Christopher Forsythe <cforsythe.cs@gmail.com>
2024-04-20 01:16:36 +0000

    Initial portfolio commit, not much done

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+// pages/index.js
+import Layout from './layout';
+
+const Home = () => {
+  return (
+    <Layout>
+      <h1>Welcome to My Portfolio</h1>
+      {/* Rest of the home page content */}
+    </Layout>
+  );
+};
+
+export default Home;
